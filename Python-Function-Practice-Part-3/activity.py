cb97a159f884aa4a1ae38623827c55c26f8cb457
John Capovani <john@capovani.com>
2023-02-11 13:33:59 -0500

    Completed activity in class

--- Python-Function-Practice-Part-3/activity.py
+++ Python-Function-Practice-Part-3/activity.py
@@ -36,3 +36,38 @@ def recursive_factorial(int):
         return (int * recursive_factorial(int - 1))
 
 print(recursive_factorial(5))
+
+# COPIED BELOW FROM SOLUTION
+
+def recursive_deduplicate(my_str, i=0):
+  # if our string is empty or only has 1 thing, it's got no duplicates
+  # if i is at the end of the string, no duplicates are left
+  if len(my_str) <= 1 or i == len(my_str)-1:
+    return my_str
+  else:
+    # str at current position is same as next position
+    if my_str[i] == my_str[i+1]:
+      return recursive_deduplicate(my_str[0:i]+my_str[i+1:], i)
+    else:
+      # no duplicate at current position, check next
+      return recursive_deduplicate(my_str, i+1)
+
+
+print(recursive_deduplicate("aaaa"))
+print(recursive_deduplicate("aaba"))
+print(recursive_deduplicate("apple"))
+print(recursive_deduplicate(""))
+
+# 6. recursive_reverse — Uses recursion to reverse a string.
+
+
+def recursive_reverse(str, i=0):
+  # empty string case
+  if len(str) == 0:
+    return ""
+  # base case
+  elif i == len(str)-1:
+    return str[0]
+  else:
+    # recursive case
+    return str[-1-i] + recursive_reverse(str, i+1)
